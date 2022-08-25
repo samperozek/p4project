e83897c950828a10b5f3146d79eb8469a80f191c
samperozek <samperozek@gmail.com>
2022-08-25 08:47:15 -0400

    added cookies actioncontroller to AppController

--- zbackend_p4/app/controllers/application_controller.rb
+++ zbackend_p4/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
     skip_before_action :verify_authenticity_token, :except => [:update, :create]
     protect_from_forgery :except => :create 
 end
